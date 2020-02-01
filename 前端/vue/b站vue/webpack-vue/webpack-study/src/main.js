147775483c1417d66664150b8ae9baf4a612e509
Lardcandy <1053649721@qq.com>
2020-02-01 16:48:11 +0800

    dell 提交

---
+++ 前端/vue/b站vue/webpack-vue/webpack-study/src/main.js
@@ -0,0 +1,15 @@
+//entrance file
+
+import Vue from 'vue'
+
+import login from './login.vue'
+
+var vm = new Vue({
+    el:'#app',
+    data:{
+        msg:'123'
+    },
+    render: function(createElements){
+        return createElements(login)
+    }
+})
---
+++ 前端/vue/b站vue/webpack-vue/webpack-study/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+// 在内存中生成html页面的插件
+// 只要是插件都要放在plugins里面
+const htmlWebpackPlugins = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+
+
+// 这个配置文件，其实就是一个js文件，通过node中的模块操作，向外暴露了一个配置对象
+module.exports = {
+    // mode:'development',
+    //配置入口 出口
+    entry:path.join(__dirname,'./src/main.js'),
+    output:{
+        path:path.join(__dirname,'./dist'),
+        filename:'bundle.js'
+    },
+    plugins:[
+        new htmlWebpackPlugins({
+            template:path.join(__dirname,'./src/index.html'),
+            filename:'index.html'
+        }),
+        new VueLoaderPlugin(),
+    ],
+    module:{ // 这个节点用于配置所有第三方模块加载器
+        rules:[
+            //所有第三方模块的匹配规则
+            {test:/\.css$/, use:['style-loader','css-loader']}, //配置处理.css文件的第三方loader规则
+            {test:/\.less$/,use:['style-loader','css-loader','less-loader']},
+            {test:/\.scss$/,use:['style-loader','css-loader','sass-loader']},
+            {test:/\.(jpg|png|gif|bmp|jpeg)$/,use:'url-loader?limit=26643&name=[name].[ext]'},
+            {test:/\.(ttf|eot|svg|woff|woff2)$/,use:'url-loader'},
+            //babel
+            {test:/\.js$/,use:'babel-loader',exclude:/node_modules/},
+            //vue
+            {test:/\.vue/,use:'vue-loader'},
+        ],
+    }
+}
